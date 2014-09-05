5c42db5dd0cff6abe620753aefe02a6edd384531
Lance Hudson <lance@lancehudson.com>
2014-09-05 13:54:51 -0400

    update docker file to start apt-cacher with log index cache and in foreground

--- setup.sh
+++ setup.sh
@@ -5,7 +5,14 @@ apt-get update
 apt-get install openssh-server git
 
 # Install Docker
-curl -sSL https://get.docker.io/ubuntu/ | sh
+echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+
+# Then import the repository key
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+
+# Install docker
+apt-get update
+apt-get install -y lxc-docker
 adduser user docker
 
 # Build Docker Images
@@ -13,7 +20,8 @@ docker build -t apt-cacher-ng AptCacher
 docker build -t npm-lazy NPMLazy
 
 
-# Install Apt-Cacher-ng
+# Install Apt-Cacher-ng - 172.17.42.1:3142
+echo 'Acquire::http { Proxy "172.17.42.1:3142"; };' >> /etc/apt/apt.conf.d/01proxy
 
 # Install NPM Lazy
 
