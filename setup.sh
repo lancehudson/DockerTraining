2ca3956b85bd5fa33276283bb688d0f632a1a882
Lance Hudson <lance@lancehudson.com>
2014-09-06 13:51:40 -0400

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,18 +10,32 @@ echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docke
 # Then import the repository key
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 
-# Install docker
+# Install Docker
 apt-get update
 apt-get install -y lxc-docker
 adduser user docker
+
+# Setup login Script
+cp Files/get-ip-address /usr/local/bin/get-ip-address
+chmod +x /usr/local/bin/get-ip-address
+cp Files/show-ip-address /etc/network/if-up.d/show-ip-address
+chmod +x /etc/network/if-up.d/show-ip-address
+cp Files/issue-standard /etc/issue-standard
+
+# Setup Init Script
+cp Files/rc.local /etc/rc.local
+chmod +x /etc/rc.local
 
 # Build Docker Images
 docker build -t apt-cacher-ng AptCacher
 docker build -t npm-lazy NPMLazy
 
-
 # Install Apt-Cacher-ng - 172.17.42.1:3142
 echo 'Acquire::http { Proxy "172.17.42.1:3142"; };' >> /etc/apt/apt.conf.d/01proxy
+docker run -d -p 3142:3142 --name apt-cacher-ng-run apt-cacher-ng
+
+# Update Apt-Cacher cache
+apt-get update
 
 # Install NPM Lazy
 
